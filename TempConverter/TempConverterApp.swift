1d598bfea2b921e2671138bb4562b0874632892b
Rob McBryde <robertmcbryde83@gmail.com>
2025-01-21 21:03:14 +0000

    Initial Commit

---
+++ TempConverter/TempConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TempConverterApp.swift
+//  TempConverter
+//
+//  Created by Rob McBryde on 21/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct TempConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
